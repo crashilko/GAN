13f862fe3460a64014a0d38d0dead1cb60989e68
crashilko <crashilko@gmail.com>
2021-11-12 22:41:34 +0300

    it works

--- gan_model.py
+++ gan_model.py
@@ -6,6 +6,10 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+    device_name = '/device:CPU:0'
+
 class GANModel:
     def __init__(self, gen_loss, gen_opt, discr_loss, discr_opt):
         self.generator, self.gen_loss, self.gen_opt = self.__init_g__(gen_loss, gen_opt)
@@ -54,43 +58,49 @@ class GANModel:
 
     def __set_architecture_g__(self):
         model = tf.keras.Sequential()
-        model.add(layers.Dense(7 * 7 * 64, use_bias=False, input_shape=(100,)))
-        #model.add(layers.BatchNormalization())
-        model.add(layers.LeakyReLU())
-
-        model.add(layers.Reshape((7, 7, 64)))
-        assert model.output_shape == (None, 7, 7, 64)  # Note: None is the batch size
-
-        model.add(layers.Conv2DTranspose(32, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-        print(model.output_shape)
-        assert model.output_shape == (None, 7, 7, 32)
+        model.add(layers.Dense(8 * 8 * 128, use_bias=False, input_shape=(100,)))
         model.add(layers.BatchNormalization())
         model.add(layers.LeakyReLU())
 
-        model.add(layers.Conv2DTranspose(16, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+        model.add(layers.Reshape((8, 8, 128)))
+        #assert model.output_shape == (None, 7, 7, 64)  # Note: None is the batch size
+
+        model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
         print(model.output_shape)
-        assert model.output_shape == (None, 14, 14, 16)
+        #assert model.output_shape == (None, 7, 7, 128)
         model.add(layers.BatchNormalization())
         model.add(layers.LeakyReLU())
 
-        model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+        model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
         print(model.output_shape)
-        assert model.output_shape == (None, 28, 28, 1)
+        #assert model.output_shape == (None, 14, 14, 64)
+        model.add(layers.BatchNormalization())
+        model.add(layers.LeakyReLU())
+
+
+        model.add(layers.Conv2DTranspose(3, (3, 3), strides=(1, 1), padding='same', use_bias=False, activation='tanh'))
+        print(model.output_shape)
+        assert model.output_shape == (None, 32, 32, 3)
         return model
 
     def __set_architecture_d__(self):
         model = tf.keras.Sequential()
-        model.add(layers.Conv2D(16, (3, 3), strides=(1, 1), padding='same',
-                                     input_shape=[28, 28, 1]))
-        model.add(layers.LeakyReLU())
-        model.add(layers.Dropout(0.3))
-
-        model.add(layers.Conv2D(16, (3, 3), strides=(2, 2), padding='same'))
-        model.add(layers.LeakyReLU())
-        model.add(layers.Dropout(0.3))
-
+        # normal
+        model.add(layers.Conv2D(64, (3,3), padding='same', input_shape=(32,32,3)))
+        model.add(layers.LeakyReLU(alpha=0.2))
+        # downsample
+        model.add(layers.Conv2D(128, (3,3), strides=(2,2), padding='same'))
+        model.add(layers.LeakyReLU(alpha=0.2))
+        # downsample
+        model.add(layers.Conv2D(128, (3,3), strides=(2,2), padding='same'))
+        model.add(layers.LeakyReLU(alpha=0.2))
+        # downsample
+        model.add(layers.Conv2D(256, (3,3), strides=(2,2), padding='same'))
+        model.add(layers.LeakyReLU(alpha=0.2))
+        # classifier
         model.add(layers.Flatten())
-        model.add(layers.Dense(1))
+        model.add(layers.Dropout(0.4))
+        model.add(layers.Dense(1, activation='sigmoid'))
 
         return model
 
@@ -114,55 +124,48 @@ class GANModel:
 
     @tf.function
     def train_step(self, images, BATCH_SIZE, noise_dim):
-        device_name = tf.test.gpu_device_name()
-        if device_name != '/device:GPU:0':
-            device_name = '/cpu:0'
         with tf.device(device_name):
+          noise = tf.random.normal([BATCH_SIZE, noise_dim])
 
-            noise = tf.random.normal([BATCH_SIZE, noise_dim])
+          with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+              generated_images = self.generator(noise, training=True)
 
-            with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-                generated_images = self.generator(noise, training=True)
+              real_output = self.discriminator(images, training=True)
+              fake_output = self.discriminator(generated_images, training=True)
 
-                real_output = self.discriminator(images, training=True)
-                fake_output = self.discriminator(generated_images, training=True)
+              gen_loss = self.generator_loss(fake_output)
+              disc_loss = self.discriminator_loss(real_output, fake_output)
+              #print("generator loss:",gen_loss, "\ndiscriminator loss:", disc_loss)
 
-                gen_loss = self.generator_loss(fake_output)
-                disc_loss = self.discriminator_loss(real_output, fake_output)
-                #print("gan_for_mnist loss:",gen_loss, "\ndiscriminator loss:", disc_loss)
+          gradients_of_generator = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
+          gradients_of_discriminator = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
 
-            gradients_of_generator = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
-            gradients_of_discriminator = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
-
-            self.gen_opt.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
-            self.discr_opt.apply_gradients(zip(gradients_of_discriminator, self.discriminator.trainable_variables))
+          self.gen_opt.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
+          self.discr_opt.apply_gradients(zip(gradients_of_discriminator, self.discriminator.trainable_variables))
 
     def train(self, dataset, epochs, BATCH_SIZE, SHOW_IMAGES, SEED = None):
-        device_name = tf.test.gpu_device_name()
-        if device_name != '/device:GPU:0':
-            device_name = '/cpu:0'
         with tf.device(device_name):
-            for epoch in range(epochs):
-                print("epoch", epoch)
-                start = time.time()
+          for epoch in range(epochs):
+              print("epoch", epoch)
+              start = time.time()
 
-                for image_batch in tqdm(dataset):
-                    self.train_step(image_batch, BATCH_SIZE, 100)
-                # Produce images for the GIF as you go
-                # display.clear_output(wait=True)
-                if SHOW_IMAGES:
-                    self.generate_and_save_images(epoch + 1, SEED)
+              for image_batch in tqdm(dataset):
+                  self.train_step(image_batch, BATCH_SIZE, 100)
+              # Produce images for the GIF as you go
+              # display.clear_output(wait=True)
+              if SHOW_IMAGES:
+                  self.generate_and_save_images(epoch + 1, SEED)
 
-                # Save the model every 15 epochs
-                if (epoch + 1) % 15 == 0:
-                    self.checkpoint.save(file_prefix=self.checkpoint_prefix)
+              # Save the model every 15 epochs
+              if (epoch + 1) % 15 == 0:
+                  self.checkpoint.save(file_prefix=self.checkpoint_prefix)
 
-                print('Time for epoch {} is {} sec'.format(epoch + 1, time.time() - start))
+              print('Time for epoch {} is {} sec'.format(epoch + 1, time.time() - start))
 
-            # Generate after the final epoch
-            # display.clear_output(wait=True)
-            if SHOW_IMAGES:
-                self.generate_and_save_images(epoch, SEED)
+          # Generate after the final epoch
+          # display.clear_output(wait=True)
+          if SHOW_IMAGES:
+              self.generate_and_save_images(epoch, SEED)
 
 
     def generate_and_save_images(self, epoch, test_input):
@@ -170,12 +173,13 @@ class GANModel:
         # This is so all layers run in inference mode (batchnorm).
         predictions = self.generator(test_input, training=False)
 
-        fig = plt.figure(figsize=(5, 5))
+        fig = plt.figure(figsize=(8, 8))
 
         for i in range(predictions.shape[0]):
-            plt.subplot(3, 3, i + 1)
-            plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+            plt.subplot(6, 6, i + 1)
+
+            plt.imshow(tf.cast(predictions[i, :, :, :]*127.5 + 127.5, dtype=tf.int32))
             plt.axis('off')
 
         plt.savefig('generated_images/image_at_epoch_{:04d}.png'.format(epoch))
-        #plt.show()
+        plt.show()
--- train.py
+++ train.py
@@ -6,21 +6,21 @@ from parser import parse
 
 
 
-model = GANModel('cross_entropy', 'Adam', 'cross_entropy', 'Adam')
-image = model.generate_image()
-plt.imshow(image[0,:,:,0])
+import numpy as np
+model2 = GANModel('cross_entropy', 'Adam', 'cross_entropy', 'Adam')
+image = model2.generate_image()
+plt.imshow(image[0,:,:,:])
 plt.show()
-num_examples_to_generate = 9
+num_examples_to_generate = 36
 noise_dim = 100
 seed = tf.random.normal([num_examples_to_generate, noise_dim])
-
-(train_images, train_labels), (_, _) = tf.keras.datasets.fashion_mnist.load_data()
-train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+(train_images, train_labels), (_, _) = tf.keras.datasets.cifar10.load_data()
+train_images = train_images.reshape(train_images.shape[0], 32, 32, 3).astype('float32')
 train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
-
 BUFFER_SIZE = 60000
 BATCH_SIZE = 32
 
-train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+train_images = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
-model.train(train_dataset, 25, 32, 'True', seed)
+
+model2.train(train_images, 100, BATCH_SIZE, 'True', seed)
